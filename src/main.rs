c869a9f8ce6b51e1d51b33edfd41d57ed8939d8f
Leon Klute <l.klute@caesar.nl>
2023-11-08 16:55:10 +0100

    reconstruction form 2d

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,7 @@
 use std::{env, fs, io};
 use netpbm;
 use wsq::swt;
+use wsq::swt::{Analysis, Synthesis};
 use wsq::swt::filter::Filter;
 
 fn parse_args() -> Result<(String, String), Box<dyn std::error::Error>> {
@@ -43,26 +44,35 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
     let pgm_image = netpbm::parser::parse(&contents)?;
     let image_copy = pgm_image.clone();
     let mut display = netpbm::display::SdlDisplay::try_new()?;
-    display.display_netpbm(&image_copy)?;
+    display.display_netpbm(&image_copy, "Original")?;
 
     let lowpass = Filter::WSS(vec![0.85269867900940, 0.37740285561265, -0.11062440441842, -0.02384946501938, 0.037828455506995]);
     let highpass = Filter::WSA(vec![0.78848561640566, -0.41809227322221, -0.040689417609558, 0.064538882628938]);
+    // let lowpass = Filter::WSS(vec![0.1, 0.1, 0.1, 0.1, 0.1]);
+    // let highpass = Filter::WSA(vec![0., 0.5, 0., 0.]);
     let subband_coder = swt::TwoChannelSubbandCoder::new(lowpass, highpass);
 
     let mut f_img = f_img_from_netpbm(&pgm_image);
     f_img.auto_normalize();
+    f_img.find_and_set_min_max();
 
     let mut a = subband_coder.analysis(&f_img)?;
     a.0.find_and_set_min_max();
     a.1.find_and_set_min_max();
-    let a0_pgm = pgm_from_float_image(&a.0);
-    let a1_pgm = pgm_from_float_image(&a.1);
-    display.display_netpbm(&a0_pgm)?;
-    display.display_netpbm(&a1_pgm)?;
-    let mut reconstructed = subband_coder.synthesis(&a.0, &a.1)?;
+    a.2.find_and_set_min_max();
+    a.3.find_and_set_min_max();
+    let a00_pgm = pgm_from_float_image(&a.0);
+    let a01_pgm = pgm_from_float_image(&a.1);
+    let a10_pgm = pgm_from_float_image(&a.2);
+    let a11_pgm = pgm_from_float_image(&a.3);
+    display.display_netpbm(&a00_pgm, "a00")?;
+    display.display_netpbm(&a01_pgm, "a01")?;
+    display.display_netpbm(&a10_pgm, "a10")?;
+    display.display_netpbm(&a11_pgm, "a11")?;
+    let mut reconstructed = subband_coder.synthesis(&a)?;
     reconstructed.find_and_set_min_max();
     let reconstructed = pgm_from_float_image(&reconstructed);
-    display.display_netpbm(&reconstructed)?;
+    display.display_netpbm(&reconstructed, "Reconstruction")?;
 
     display.wait_for_exit();
     Ok(())
--- src/swt.rs
+++ src/swt.rs
@@ -1,3 +1,4 @@
+use std::error::Error;
 use std::slice;
 
 pub mod filter;
@@ -16,7 +17,7 @@ impl From<Vec<Vec<f64>>> for FloatImage {
     fn from(value: Vec<Vec<f64>>) -> Self {
         let width = value[0].len();
         let height = value.len();
-        let data = value.iter().flatten().map(|f| *f).collect();
+        let data = value.into_iter().flatten().collect::<Vec<f64>>();
         FloatImage {
             data,
             width,
@@ -27,7 +28,45 @@ impl From<Vec<Vec<f64>>> for FloatImage {
     }
 }
 
+struct Columns<'a, F> {
+    data: &'a [F],
+    width: usize,
+    col: usize,
+}
+
+impl<'a, F> Columns<'a, F> {
+    fn new(data: &'a [F], width: usize) -> Self {
+        Self {
+            data,
+            width,
+            col: 0,
+        }
+    }
+}
+
+impl<T> Iterator for Columns<'_, T> where T: Copy {
+    type Item = Vec<T>;
+
+    fn next(&mut self) -> Option<Self::Item> {
+        let mut col = vec![];
+        if self.col >= self.width {
+            return None;
+        }
+        for i in (0..self.data.len()).step_by(self.width) {
+            col.push(self.data[i + self.col]);
+        }
+        self.col += 1;
+        Some(col)
+    }
+}
+
 impl FloatImage {
+    pub fn rotate(&mut self) {
+        self.data = self.columns().flatten().collect();
+        let tmp = self.width;
+        self.width = self.height;
+        self.height = tmp;
+    }
     pub fn get_mean_and_rescale(&self) -> (f64, f64) {
         let mean = self.data.iter().sum::<f64>() / self.data.len() as f64;
         let rescale = f64::max(self.max_value - mean, mean - self.min_value) / 128.;
@@ -36,6 +75,7 @@ impl FloatImage {
 
     pub fn auto_normalize(&mut self) {
         let (mean, rescale) = self.get_mean_and_rescale();
+        log::debug!("Normalizing image with M:{} and R:{}", mean, rescale);
         self.normalize(mean, rescale)
     }
 
@@ -44,9 +84,15 @@ impl FloatImage {
             *f = (*f - mean) / rescale;
         }
     }
+
+    fn columns(&self) -> Columns<'_, f64> {
+        Columns::new(&self.data, self.width)
+    }
+
     fn rows(&self) -> slice::Chunks<'_, f64> {
         self.data.chunks(self.width)
     }
+
     pub fn find_min_max(&self) -> (f64, f64) {
         let mut min = f64::MAX;
         let mut max = f64::MIN;
@@ -83,23 +129,44 @@ impl<F> TwoChannelSubbandCoder<F>
 }
 
 impl TwoChannelSubbandCoder<f64> {
-    fn analysis_1d(&self, signal: &[f64]) -> (Vec<f64>, Vec<f64>) {
-        let lowpassed = self.h_lowpass.apply(signal);
-        let highpassed = self.h_highpass.apply(signal);
-        let lowpassed = Self::downsample(&lowpassed);
-        let highpassed = Self::downsample(&highpassed);
-        (lowpassed, highpassed)
+    fn downsample(signal: &[f64]) -> Vec<f64> {
+        let mut downsampled = Vec::with_capacity(signal.len() / 2);
+        // signal.into_iter().step_by(2).collect()
+        signal.iter().step_by(2).for_each(|s| downsampled.push(*s));
+        downsampled
     }
 
-    fn synthesis_1d(&self, a_0: &[f64], a_1: &[f64]) -> Vec<f64> {
-        let a_0 = Self::upsample(a_0);
-        let a_1 = Self::upsample(a_1);
-        let x_hat_0 = self.f_lowpass.apply(&a_0);
-        let x_hat_1 = self.f_highpass.apply(&a_1);
-        x_hat_0.iter().zip(x_hat_1).map(|(x_0, x_1)| *x_0 + x_1).collect()
+    fn upsample(signal: &[f64]) -> Vec<f64> {
+        let mut result = vec![f64::default(); signal.len() * 2];
+        for (i, f) in signal.iter().enumerate() {
+            result[i * 2] = *f;
+        }
+        result
     }
+}
 
-    pub fn analysis(&self, image: &FloatImage) -> Result<(FloatImage, FloatImage), Box<dyn std::error::Error>> {
+pub trait Analysis {
+    fn analysis(&self, image: &FloatImage) -> Result<(FloatImage, FloatImage, FloatImage, FloatImage), Box<dyn Error>>;
+    fn row_analysis(&self, image: &FloatImage) -> Result<(FloatImage, FloatImage), Box<dyn std::error::Error>>;
+    fn column_analysis(&self, image: &FloatImage) -> Result<(FloatImage, FloatImage), Box<dyn std::error::Error>>;
+    fn analysis_1d(&self, signal: &[f64]) -> (Vec<f64>, Vec<f64>);
+}
+
+pub trait Synthesis {
+    fn synthesis_1d(&self, a_0: &[f64], a_1: &[f64]) -> Vec<f64>;
+    fn synthesis(&self, a: &(FloatImage, FloatImage, FloatImage, FloatImage)) -> Result<FloatImage, Box<dyn Error>>;
+    fn row_synthesis(&self, image_lowpass: &FloatImage, image_highpass: &FloatImage) -> Result<FloatImage, Box<dyn Error>>;
+    fn column_synthesis(&self, image_lowpass: &FloatImage, image_highpass: &FloatImage) -> Result<FloatImage, Box<dyn Error>>;
+}
+
+impl Analysis for TwoChannelSubbandCoder<f64> {
+    fn analysis(&self, image: &FloatImage) -> Result<(FloatImage, FloatImage, FloatImage, FloatImage), Box<dyn Error>> {
+        let a_row = self.row_analysis(image)?;
+        let a_0 = self.column_analysis(&a_row.0)?;
+        let a_1 = self.column_analysis(&a_row.1)?;
+        Ok((a_0.0, a_0.1, a_1.0, a_1.1))
+    }
+    fn row_analysis(&self, image: &FloatImage) -> Result<(FloatImage, FloatImage), Box<dyn std::error::Error>> {
         let mut rows_lowpass = vec![];
         let mut rows_highpass = vec![];
 
@@ -108,16 +175,52 @@ impl TwoChannelSubbandCoder<f64> {
             rows_lowpass.push(lowpassed);
             rows_highpass.push(highpassed);
         }
-        let mut lowpass_image = FloatImage::from(rows_lowpass);
-        lowpass_image.min_value = image.min_value;
-        lowpass_image.max_value = image.max_value;
-        let mut highpass_image = FloatImage::from(rows_highpass);
-        highpass_image.min_value = image.min_value;
-        highpass_image.max_value = image.max_value;
-        Ok((lowpass_image, highpass_image))
+        let rows_lowpass_image = FloatImage::from(rows_lowpass);
+        let rows_highpass_image = FloatImage::from(rows_highpass);
+        Ok((rows_lowpass_image, rows_highpass_image))
+    }
+    fn column_analysis(&self, image: &FloatImage) -> Result<(FloatImage, FloatImage), Box<dyn std::error::Error>> {
+        let mut cols_lowpass = vec![];
+        let mut cols_highpass = vec![];
+
+        for col in image.columns() {
+            let (lowpassed, highpassed) = self.analysis_1d(&col);
+            cols_lowpass.push(lowpassed);
+            cols_highpass.push(highpassed);
+        }
+        let mut rows_lowpass_image = FloatImage::from(cols_lowpass);
+        let mut rows_highpass_image = FloatImage::from(cols_highpass);
+        rows_lowpass_image.rotate();
+        rows_highpass_image.rotate();
+        Ok((rows_lowpass_image, rows_highpass_image))
     }
 
-    pub fn synthesis(&self, image_lowpass: &FloatImage, image_highpass: &FloatImage) -> Result<FloatImage, Box<dyn std::error::Error>> {
+    fn analysis_1d(&self, signal: &[f64]) -> (Vec<f64>, Vec<f64>) {
+        let lowpassed = self.h_lowpass.apply(signal);
+        let highpassed = self.h_highpass.apply(signal);
+        let lowpassed = Self::downsample(&lowpassed);
+        let highpassed = Self::downsample(&highpassed);
+        (lowpassed, highpassed)
+    }
+}
+
+impl Synthesis for TwoChannelSubbandCoder<f64> {
+    fn synthesis_1d(&self, a_0: &[f64], a_1: &[f64]) -> Vec<f64> {
+        let a_0 = Self::upsample(a_0);
+        let a_1 = Self::upsample(a_1);
+        let x_hat_0 = self.f_lowpass.apply(&a_0);
+        let x_hat_1 = self.f_highpass.apply(&a_1);
+        x_hat_0.iter().zip(x_hat_1).map(|(x_0, x_1)| *x_0 + x_1).collect()
+    }
+
+
+    fn synthesis(&self, a: &(FloatImage, FloatImage, FloatImage, FloatImage)) -> Result<FloatImage, Box<dyn Error>> {
+        let y_0 = self.row_synthesis(&a.0, &a.1)?;
+        let y_1 = self.row_synthesis(&a.2, &a.3)?;
+        let x_hat = self.column_synthesis(&y_0, &y_1)?;
+        Ok(x_hat)
+    }
+    fn row_synthesis(&self, image_lowpass: &FloatImage, image_highpass: &FloatImage) -> Result<FloatImage, Box<dyn Error>> {
         let mut data = vec![];
 
         for (a_0, a_1) in image_lowpass.rows().zip(image_highpass.rows()) {
@@ -130,17 +233,34 @@ impl TwoChannelSubbandCoder<f64> {
         Ok(result)
     }
 
-    fn downsample(signal: &[f64]) -> Vec<f64> {
-        let mut downsampled = Vec::with_capacity(signal.len() / 2);
-        signal.iter().step_by(2).for_each(|s| downsampled.push(*s));
-        downsampled
-    }
+    fn column_synthesis(&self, image_lowpass: &FloatImage, image_highpass: &FloatImage) -> Result<FloatImage, Box<dyn Error>> {
+        let mut data = vec![];
 
-    fn upsample(signal: &[f64]) -> Vec<f64> {
-        let mut result = vec![f64::default(); signal.len() * 2];
-        for (i, f) in signal.iter().enumerate() {
-            result[i * 2] = *f;
+        for (a_0, a_1) in image_lowpass.columns().zip(image_highpass.columns()) {
+            let reconstructed = self.synthesis_1d(&a_0, &a_1);
+            data.push(reconstructed);
         }
-        result
+
+        let mut result = FloatImage::from(data);
+        result.rotate();
+        result.max_value = (image_highpass.max_value + image_lowpass.max_value) / 2.;
+        Ok(result)
+    }
+}
+
+
+#[cfg(test)]
+mod tests {
+    use crate::swt::FloatImage;
+
+    #[test]
+    fn test_columns() {
+        let image = FloatImage::from(vec![vec![1., 2., 3., 4., 5.], vec![6., 7., 8., 9., 0.], vec![1., 2., 3., 4., 5.], vec![6., 7., 8., 9., 0.], vec![1., 2., 3., 4., 5.]]);
+        let mut cols = image.columns();
+        assert_eq!(Some(vec![1., 6., 1., 6., 1.]), cols.next());
+        assert_eq!(Some(vec![2., 7., 2., 7., 2.]), cols.next());
+        assert_eq!(Some(vec![3., 8., 3., 8., 3.]), cols.next());
+        assert_eq!(Some(vec![4., 9., 4., 9., 4.]), cols.next());
+        assert_eq!(Some(vec![5., 0., 5., 0., 5.]), cols.next());
     }
 }
